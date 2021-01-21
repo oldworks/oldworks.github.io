e72c81b318d5e56809fa536d1eeb51a72e323065
oldworks <54027976+oldworks@users.noreply.github.com>
2021-01-21 17:09:26 -0400

    Add files via upload

---
+++ js/glitch.js
@@ -0,0 +1,59 @@
+if(window.innerWidth >= 768) {
+
+  (function($) {
+    //******** GLITCH IMAGE HOVER ***********
+
+    var data
+
+    $('.project-hover').mouseenter(function() {
+      var img, maxGlitch, offset;
+
+      img = $(this).find('.glitch-hover');
+      data = img.attr('src');
+      protectedOffset = 0;
+      fps = 10;
+      maxMaxGlitch = 0;
+      offset = 5;
+      minNoGlitchChance = 1.0;
+      maxGlitch = 1;
+      noGlitchChance = 2;
+
+      icount = 0;
+      protectedData = data.substring(0, protectedOffset);
+      noGlitchChanceStart = noGlitchChance * 1;
+
+      window.glitchInterval = setInterval((function() {
+        var corrupted, glitch, i, j, p, ref;
+        corrupted = data;
+        icount = icount + 1;
+        if (maxGlitch < maxMaxGlitch) {
+          maxGlitch = maxGlitch * 1.01;
+        }
+        if (noGlitchChance > minNoGlitchChance) {
+          noGlitchChance = (noGlitchChanceStart - Math.pow(1.012, icount)) / 10000;
+        }
+        if (Math.random() > noGlitchChance) {
+          glitch = Math.round(Math.random() * maxGlitch);
+          for (i = j = 0, ref = glitch; 0 <= ref ? j < ref : j > ref; i = 0 <= ref ? ++j : --j) {
+            p = offset + Math.round(Math.random() * (corrupted.length - offset - 1));
+            corrupted = corrupted.substr(0, p) + corrupted.charAt(p + 1) + corrupted.charAt(p) + corrupted.substr(p + 2);
+          }
+        }
+        corruptedPatched = protectedData + corrupted.substring(protectedOffset, corrupted.length - 6) + "//2Q==";
+        return img.attr('src', corruptedPatched);
+      }), 1000 / fps);
+
+    })
+
+
+    //******** START MOUSELEAVE  ***********
+
+
+    $('.project-hover').mouseleave(function() {
+      if(window.glitchInterval){
+        clearInterval(window.glitchInterval)
+      }
+      $(this).find('.glitch-hover').attr('src', data)
+    })
+  })(jQuery);
+}
---
+++ js/global.js
@@ -0,0 +1,130 @@
+if ( /^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {
+  window.onpageshow = function(event) {
+    if (event.persisted) {
+        $('body').css({ display: 'flex' });
+    }
+  };
+}
+
+class Viewport {
+  constructor(opts) {
+
+    this.observer = new IntersectionObserver(this.cb.bind(this), {
+      threshold: [0.05, 0.25, 0.5, 0.75, 1.0]
+    })
+
+    this.lazyObserver = new IntersectionObserver(this.preload.bind(this), {
+      rootMargin: '500px 0px',
+      threshold: [0.1, 0.25, 0.5, 0.75, 1.0]
+    })
+
+    this.lazies = Array.from(document.querySelectorAll('.lazy'))
+    this.observables = Array.from(document.querySelectorAll('.observable'))
+
+    this.init()
+  }
+
+  init() {
+    this.observables.map(observable => {
+      this.observer.observe(observable)
+    })
+
+    this.lazies.map(observable => {
+      this.lazyObserver.observe(observable)
+    })
+  }
+
+  preload(entries) {
+    entries.forEach(entry => {
+      if (entry.intersectionRatio > 0) {
+        let target = entry.target
+        target.setAttribute('src', target.getAttribute('data-src'))
+        this.lazyObserver.unobserve(target)
+      }
+    })
+  }
+
+  
+  cb(entries) {
+    entries.forEach(entry => {
+      if (entry.intersectionRatio > 0) { 
+        this.observe(entry.target, entry.intersectionRatio)
+      }
+    })
+  }
+
+  observe(target, ratio) {
+    if (target.classList.contains('observable') && !target.classList.contains('observed')) {
+      target.classList.add('observed');
+    }
+
+
+    if (target.nodeName === 'VIDEO') {
+      ratio <= 0.25 && !target.paused ? target.pause() : target.play()
+    }
+  }
+}
+
+function checkScroll() {
+  $('#back-to-top').css('opacity', window.scrollY > 720 ? 1 : 0)
+}
+
+// Document.Ready
+$(function() {
+  checkScroll()
+
+  if(window.innerWidth >= 768) {
+
+    // --- Parallax ---
+    var controller = new ScrollMagic.Controller();
+
+    $('.parallax-1').each(function(){
+        var tween = new TimelineMax();
+
+        tween.from($(this), 1, {y: '10%', ease: Power0.easeNone});
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: this,
+            triggerHook: 1,
+            duration: '75%',
+        }).setTween(tween).addTo(controller);
+    });
+
+
+    $('.parallax-2').each(function(){
+        var tween = new TimelineMax();
+
+        tween.from($(this), 1, {y: '-10%', ease: Power0.easeNone});
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: this,
+            triggerHook: 1,
+            duration: '175%',
+        }).setTween(tween).addTo(controller);
+    });
+  }
+
+  $(window).scroll(checkScroll);
+
+  $('#back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: '0'
+    }, 1250, 'swing');
+  });
+
+  $('a').click(function(event) {
+    event.preventDefault();
+    newLocation = this.href;
+
+    $('body').fadeOut(500, function() {
+      window.location = newLocation;
+    });
+  });
+
+});
+
+window.addEventListener('load', function() {
+  if (('IntersectionObserver' in window)) {
+    new Viewport();
+  }
+});
